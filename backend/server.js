7fdee4d3c04d6380ad21e1983f538157a955f6e8
jimmypuli <armg296@gmail.com>
2025-02-18 16:14:10 -0500

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express'; 
+import authRoutes from "./routes/auth.routes.js"; 
 
 const app = express(); 
 
-app.get("/", (req, res) => {
-    res.send("Server is ready");
-})
+// app.get("/", (req, res) => {
+//     res.send("Server is ready");
+// })
+
+app.use("/api/auth", authRoutes); 
 
 app.listen(5000, ()=> {
     console.log("Server started at http://localhost:5000");
