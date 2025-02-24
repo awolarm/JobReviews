b322c944323c61f72dbadee997fda4575bfdfc61
jimmypuli <armg296@gmail.com>
2025-02-24 14:53:40 -0500

    optimized dependancies
    changed chakra version to fix styling issues

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,15 @@
+import { Button, Stack } from '@chakra-ui/react'
+
 const HomePage = () => {
     return(
-        <div>HomePage</div>
+        <div>
+            <h1>Homepage</h1>
+            <Stack spacing={4} direction='row' align='center'>
+                <Button colorScheme='teal' size='lg'>
+                    Click Me
+                </Button>
+            </Stack>
+        </div>
     )
 }
 
