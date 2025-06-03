de262b0adb3b1eb12cffebfc1e069024956ec5b6
jimmypuli <armg296@gmail.com>
2025-06-03 11:03:24 -0400

    Scaling program

--- webscraper/sb.py
+++ webscraper/sb.py
@@ -8,7 +8,7 @@ with SB(uc=True, test=True, locale="en") as sb:
     url = "https://www.indeed.com/cmp/Walmart/reviews"
     sb.activate_cdp_mode(url)
     sb.uc_gui_click_captcha()
-    sb.sleep(2)
+    sb.sleep(5)
     page_html = sb.get_page_source()
     soup = BeautifulSoup(page_html, 'html.parser')
 
@@ -16,12 +16,20 @@ with SB(uc=True, test=True, locale="en") as sb:
     
     for child in reviewList: 
         spans = child.find_all('span', class_='css-15r9gu1 eu4oa1w0')
+        spans_date = child.find('span', class_='css-1tqkbyy e1wnkr790') 
+        spans_location = child.find('span', class_='css-7egqgm e1wnkr790')
+        h2_role = child.find('h2', class_='css-1fvr0l2 e1tiznh50')
+        if not h2_role: 
+           h2_role = child.find('h2', class_='css-p5j9gb e1tiznh50')
         if len(spans) >= 2: 
             title = spans[0]
             description = spans[1]
             review = {
                 'title' : title.text.strip(),
                 'description': description.text.strip(),
+                'date': spans_date.text.strip() if spans_date else 'N/A',
+                'role': h2_role.text.strip() if h2_role else 'N/A',
+                'location': spans_location.text.strip() if spans_location else 'N/A',
                 'company' : 'Walmart', 
             }
             reviews_data.append(review)
